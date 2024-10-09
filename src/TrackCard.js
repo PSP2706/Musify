7bf7f5b3c35e6b704629604ffc474c1f3ec5bc18
PSP2706 <144992405+PSP2706@users.noreply.github.com>
2024-10-09 11:32:40 +0530

    Fixed bigs in Trackcard.js. Ensured two songs don't play at the same time.

--- src/TrackCard.js
+++ src/TrackCard.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
+let currentAudio = null; 
 
 export default function TrackCard({ track }) {
+  const audioRef = useRef(null); 
+
+  const handlePlay = () => {
+    if (currentAudio && currentAudio !== audioRef.current) {
+      currentAudio.pause(); 
+    }
+    currentAudio = audioRef.current; 
+  };
+
+  const handlePause = () => {
+    if (currentAudio === audioRef.current) {
+      currentAudio = null; 
+    }
+  };
+
   return (
     <div className='col-lg-3 col-md-6 py-2'>
-      <div className="card" >
+      <div className="card">
         <img src={track.album.images[1].url} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{track.name} - {track.album.name}</h5>
           <p className="card-text">
             {track.album.artists[0].name}
           </p>
-          <audio src={track.preview_url} controls className='w-100'></audio>
+          <audio
+            ref={audioRef}
+            src={track.preview_url}
+            controls
+            className='w-100'
+            onPlay={handlePlay}
+            onPause={handlePause} 
+          ></audio>
         </div>
       </div>
     </div>
